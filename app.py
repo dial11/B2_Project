a0baf57d88e0b0f76754be0935eac09cd72ecdda
Jeongjiw00 <wldn0229@naver.com>
2022-12-02 21:26:30 +0900

    image,css,js파일생성/app.py/config.py생성

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request, jsonify
+from sqlalchemy import create_engine, text
+app = Flask(__name__)
+
+@app.route('/login')
+def home():
+    return render_template('login.html')
+
+# @app.route('/api/user/post', methods=['POST'])
+# def save_user():
+
+#     # userName = {'name': request.form['name']}
+
+#     userName = request.form['name']
+
+#     sql = 'INSERT INTO user(name) VALUES(%s)'
+
+#     app.database.execute(sql, (userName)).lastrowid #실행한 테이블의 마지막 행 아이디를 가져옴.
+
+#     return jsonify({'msg':'등록성공'})
+
+if __name__ == '__main__':
+    app.config.from_pyfile("config.py")
+    database = create_engine(app.config['DB_URL'], encoding='utf-8', max_overflow=0)
+    app.database = database
+
+    app.run('0.0.0.0', port=5000, debug=True)
---
+++ config.py
@@ -0,0 +1,14 @@
+db = {
+    # 데이터베이스에 접속할 사용자 아이디
+    'user': 'project2b2',
+    # 사용자 비밀번호
+    'password': 'project2b2',
+    # 접속할 데이터베이스의 주소 (같은 컴퓨터에 있는 데이터베이스에 접속하기 때문에 localhost)
+    'host': '182.212.65.173',
+    # 관계형 데이터베이스는 주로 3306 포트를 통해 연결됨
+    'port': 3306,
+    # 실제 사용할 데이터베이스 이름
+    'database': 'project2b2'
+}
+
+DB_URL = f"mysql+mysqlconnector://{db['user']}:{db['password']}@{db['host']}:{db['port']}/{db['database']}?charset=utf8"
