86f72d1889aab2384c2bf3a120a18b2cd7a5f44b
go-tiger <inter_tiger_@naver.com>
2022-12-03 04:05:50 +0900

    modity app0621.py

--- app0621.py
+++ app0621.py
@@ -1,4 +1,27 @@
 from flask import Flask, render_template, request, jsonify
+import pymysql
+
+db = pymysql.connect(host="182.212.65.173", user="project2b2", passwd="project2b2", db="project2b2", charset="utf8")
+cur = db.cursor()
+
+sql = "SELECT * from category"
+cur.execute(sql)
+
+data_list = cur.fetchone()
+data_list2 = cur.fetchall()
+
+print(data_list)
+print(data_list[0])
+print(data_list[1])
+
+print('--------------')
+
+print(data_list2)
+print(data_list2[0])
+print(data_list2[0][0])
+print(data_list2[0][1])
+
+
 app = Flask(__name__)
 
 @app.route('/')
