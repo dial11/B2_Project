6c693735219a164cfa4d838dcf19a936b6d5a826
go-tiger <inter_tiger_@naver.com>
2022-12-04 05:13:43 +0900

    modity userdit.css/js/html

--- static/js/useredit.js
+++ static/js/useredit.js
@@ -1,3 +1,42 @@
 $(document).ready(function () {
-    show_comment();
+    get_user();
 });
+function get_user() {
+    $.ajax({
+        type: "GET",
+        url: "/api/user",
+        data: {},
+        success: function (response) {
+            let rows = response["msg"];
+
+            for (let i = 0; i < rows.length; i++) {
+                let name = rows[i]["name"];
+                let email = rows[i]["email"];
+                console.log(name);
+
+                let temp_html = `
+                    ${name}
+                    ${email}
+                `;
+
+                $("#user-list").append(temp_html);
+            }
+        },
+    });
+}
+
+// 유저 저장
+function save_user() {
+    let userName = $("#user-name").val();
+    let userEmail = $("#user-email").val();
+
+    $.ajax({
+        type: "POST",
+        url: "/api/user",
+        data: { name: userName, email: userEmail },
+        success: function (response) {
+            alert(response["msg"]);
+            window.location.reload();
+        },
+    });
+}
