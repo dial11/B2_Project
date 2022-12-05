7ee78e8f2028aabdd757804122d61a36408392b9
Jeongjiw00 <wldn0229@naver.com>
2022-12-06 03:01:17 +0900

    포스트(분야,제목,내용)

--- app_jw.py
+++ app_jw.py
@@ -24,17 +24,105 @@ db = pymysql.connect (
 
 curs = db.cursor()
 
-
+# 준기님코드합침
 @app.route('/')
 def root():
-    return render_template('index.html')
+    print('show_all')
+    return render_template('index.html', category_name='all')
+
+@app.route('/logout',methods=['GET'])
+def logout():
+    session.pop('id', None)
+    return redirect('/')
+
+@app.route('/category-backend')
+def backendLayout():
+    print('show_backend')
+    return render_template('index.html', category_name='backend')
+
+
+@app.route('/category-frontend')
+def frontendLayout():
+    print('front_backend')
+    return render_template('index.html', category_name='frontend')
+
+@app.route('/user', methods=['GET'])
+def get_users():
+    print('get_users')
+    db = pymysql.connect(
+        user='project2b2',
+        password='project2b2',
+        host='182.212.65.173',
+        port=3306,
+        database='project2b2',
+        charset='utf8'
+    )
+
+    curs = db.cursor()
+
+    sql = """
+        SELECT u.email, u.password, u.name, u.description
+        FROM user u
+        """
+
+    curs.execute(sql)
+    rows_user = curs.fetchall()
+    print(rows_user)
+
+    json_str = json.dumps(rows_user, indent=4, sort_keys=True, default=str)
+
+    db.commit()
+    db.close()
+
+    return json_str, 200
+
+
+@app.route('/board', defaults={'page': 1})
+@app.route('/board/<int:page>')
+def get_boards(page):
+    print('get_boards')
+    db = pymysql.connect(
+        user='project2b2',
+        password='project2b2',
+        host='182.212.65.173',
+        port=3306,
+        database='project2b2',
+        charset='utf8'
+    )
+
+    curs = db.cursor()
+
+    perpage = 5
+    startat = (page - 1) * perpage
+
+    sql = f"""
+        SELECT b.title, b.content, b.created_at, u.name, c.name
+        FROM board b
+        INNER JOIN `user` u
+        ON b.user_id = u.id
+        INNER JOIN category c
+        ON b.category_id = c.id
+        ORDER BY b.id desc
+        LIMIT {perpage}
+        OFFSET {startat}
+        """
+
+    curs.execute(sql)
+    rows_board = curs.fetchall()
+    print(rows_board)
+
+    json_str = json.dumps(rows_board, indent=4, sort_keys=True, default=str)
+
+    db.commit()
+    db.close()
+
+    return json_str, 200
+
 
 # 로그인/회원가입페이지로 이동
 @app.route('/login')
 def login():
     return render_template('login.html')
-
-
 # 글작성페이지로 이동
 @app.route('/post')
 def post():
@@ -88,8 +176,25 @@ def user_login():
             session['name'] = result[2]
             session['email'] = result[3]
             return jsonify({'msg':'로그인 성공'})
-        
-   
+
+# 게시글 등록하기
+@app.route('/write', methods=['POST'])
+def post_board():
+    selectPost = request.form['category_id']
+    postTitle = request.form['title']
+    postContent = request.form['content']
+    userId = session['id']
+    # postFile = request.form['data']
+    # userName = request.form['name']
+
+    sql1 = f'INSERT INTO project2b2.board(title,content,category_id,user_id) VALUES({postTitle},{postContent},{selectPost},{userId})'
+    # sql2 = f'INSERT INTO project2b2.board(data) VALUES(LOAD_FILE("{postFile}"))'
+    
+    curs.execute(sql1) # 데이터베이스에 넣어주기 위함
+    # curs.execute(sql2)
+    db.commit() #삽입,삭제,수정할때, 최종적으로 데이터베이스를 만져줄때만
+
+    return jsonify({'msg':'POST 성공'})
 
 
 
