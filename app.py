b4a024608d06417bd748c2d484fa9294b426cbb2
Joon Ki Chun <joon0858@naver.com>
2022-12-05 10:51:30 +0900

    Update category별 get 기능

--- app.py
+++ app.py
@@ -1,27 +1,78 @@
-from flask import Flask, render_template, request, jsonify
-from sqlalchemy import create_engine, text
+from flask import Flask, render_template
+import pymysql
+import json
+
 app = Flask(__name__)
 
-@app.route('/login')
-def home():
-    return render_template('login.html')
 
-# @app.route('/api/user/post', methods=['POST'])
-# def save_user():
+@app.route('/')
+def root():
+    print('show_all')
+    return render_template('index.html', category_name='all')
 
-#     # userName = {'name': request.form['name']}
 
-#     userName = request.form['name']
+@app.route('/category-backend')
+def backendLayout():
+    print('show_backend')
+    return render_template('index.html', category_name='backend')
 
-#     sql = 'INSERT INTO user(name) VALUES(%s)'
 
-#     app.database.execute(sql, (userName)).lastrowid #실행한 테이블의 마지막 행 아이디를 가져옴.
+@app.route('/category-frontend')
+def frontendLayout():
+    print('front_backend')
+    return render_template('index.html', category_name='frontend')
 
-#     return jsonify({'msg':'등록성공'})
+
+@app.route('/user', methods=['GET'])
+def get_users():
+    print('get_users')
+    db = pymysql.connect(host='localhost', user='root', db='project2b2', password='10041004',
+                         charset='utf8')
+    curs = db.cursor()
+
+    sql_user = """
+        SELECT u.email, u.password, u.name, u.description
+        FROM user u
+        """
+
+    curs.execute(sql_user)
+    rows_user = curs.fetchall()
+    print(rows_user)
+
+    json_str = json.dumps(rows_user, indent=4, sort_keys=True, default=str)
+
+    db.commit()
+    db.close()
+    return json_str, 200
+
+
+@app.route('/board', methods=['GET'])
+def get_boards():
+    print('get_boards')
+    db = pymysql.connect(host='localhost', user='root', db='project2b2', password='10041004',
+                         charset='utf8')
+    curs = db.cursor()
+
+    sql_board = """
+        SELECT b.title, b.content, b.created_at, u.name, c.name
+        FROM board b
+        INNER JOIN `user` u
+        ON b.user_id = u.id
+        INNER JOIN category c
+        ON b.category_id = c.id
+        ORDER BY b.id
+        """
+
+    curs.execute(sql_board)
+    rows_board = curs.fetchall()
+    print(rows_board)
+
+    json_str = json.dumps(rows_board, indent=4, sort_keys=True, default=str)
+
+    db.commit()
+    db.close()
+    return json_str, 200
+
 
 if __name__ == '__main__':
-    app.config.from_pyfile("config.py")
-    database = create_engine(app.config['DB_URL'], encoding='utf-8', max_overflow=0)
-    app.database = database
-
-    app.run('0.0.0.0', port=5000, debug=True)
+    app.run(host='127.0.0.1', port=5000, debug=True)
--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,41 +1,193 @@
 $(document).ready(function () {
-    show_comment();
+    showRandomUser();
+    showBoard();
+    showBackendBoard();
+    showFrontendBoard();
 });
 
+function pickThreeNum (user_length) {
+  let num_list = [];
+  let i = 0;
+  while (i < 3) {
+    let n = Math.floor(Math.random() * user_length);
+    if (! same_num(n)) {
+      num_list.push(n);
+      i++;
+    }
+  }
+  function same_num (n) {
+    for (let i = 0; i < num_list.length; i++) {
+      if (n === num_list[i]) {
+        return true;
+      }
+    }
+    return false;
+  }
+  return num_list;
+}
 
-function show_comment() {
-    $('#board-list').empty()
+function showRandomUser() {
+    $('#random-user').empty()
+    $.ajax({
+        type: "GET",
+        url: "/user",
+        data: {},
+        success: function (response) {
+            console.log('success_showRandomUser')
+            let user_list = JSON.parse(response)
+            let rand_num_list = pickThreeNum(user_list.length)
+            for (let i = 0; i < 3; i++) {
+                let rand_num = rand_num_list[i];
+                // let email = user_list[rand_num][0]
+                // let password = user_list[rand_num][1]
+                let name = user_list[rand_num][2]
+                let description = user_list[rand_num][3]
+                let temp_html = `
+                                <div class="card mb-3" style="max-width: 540px; margin-right: 30px">
+                                    <div class="row g-0">
+                                        <div class="col-md-3">
+                                            <img alt="이미지가 없습니다" class="img-fluid rounded-start" style="height: 70px;"
+                                                 src="/static/images/default_image.png">
+                                        </div>
+                                        <div class="col-md-9">
+                                            <div class="card-body">
+                                                <h5 class="card-title">${name}</h5>
+                                                <p class="card-text">${description}</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                `
+                $('#random-user').append(temp_html)
+            }
+        }
+    });
+}
+
+function showBoard() {
+    $('#all-board-list').empty()
     $.ajax({
         type: "GET",
         url: "/board",
         data: {},
         success: function (response) {
-            console.log('success')
+            console.log('success_showBoard')
             let board_list = JSON.parse(response)
             for (let i = 0; i < board_list.length; i++) {
                 let name = board_list[i][0]
                 let content = board_list[i][1]
                 let time = board_list[i][2]
+                let user_name = board_list[i][3]
                 let temp_html = `
-                                <div style="display: flex; align-items: center;">
-                                        <img alt="이미지가 없습니다" class="img-fluid rounded-start"
-                                             src="/static/images/default_image.png"
-                                             style="height: 40px;">
-                                        <span>{user_name}</span> 
-                                </div>
-                                <div class="card" style="margin-bottom: 10px;">
-                                    <div class="card-body">
-                                        <blockquote class="blockquote mb-0" style="height: 120px;">
-                                            <div style="margin-bottom: 10px;">
-                                                <span>${name}</span> 
-                                                <span style="text-align: right; font-size: 12px">(${time})</span>
-                                            </div>
-                                            <span>${content}</span>
-                                        </blockquote>
+                                <div>
+                                    <div style="display: flex; align-items: center;">
+                                            <img alt="이미지가 없습니다" class="img-fluid rounded-start"
+                                                 src="/static/images/default_image.png"
+                                                 style="height: 40px; margin-right: 10px;">
+                                            <span>${user_name}</span> 
+                                    </div>
+                                    <div class="card" style="margin-bottom: 10px;">
+                                        <div class="card-body">
+                                            <blockquote class="blockquote mb-0" style="height: 120px;">
+                                                <div style="margin-bottom: 10px;">
+                                                    <span>${name}</span> 
+                                                    <span style="text-align: right; font-size: 12px">(${time})</span>
+                                                </div>
+                                                <span>${content}</span>
+                                            </blockquote>
+                                        </div>
                                     </div>
                                 </div>
                                 `
-                $('#board-list').prepend(temp_html)
+                $('#all-board-list').prepend(temp_html)
+            }
+        }
+    });
+}
+
+function showBackendBoard() {
+    $('#backend-board-list').empty()
+    $.ajax({
+        type: "GET",
+        url: "/board",
+        data: {},
+        success: function (response) {
+            console.log('success_showBackendBoard')
+            let board_list = JSON.parse(response)
+            for (let i = 0; i < board_list.length; i++) {
+                let name = board_list[i][0]
+                let content = board_list[i][1]
+                let time = board_list[i][2]
+                let user_name = board_list[i][3]
+                let category_name = board_list[i][4]
+                if (category_name === '백엔드 개발자') {
+                    let temp_html = `
+                                    <div>
+                                        <div style="display: flex; align-items: center;">
+                                                <img alt="이미지가 없습니다" class="img-fluid rounded-start"
+                                                     src="/static/images/default_image.png"
+                                                     style="height: 40px; margin-right: 10px;">
+                                                <span>${user_name}</span> 
+                                        </div>
+                                        <div class="card" style="margin-bottom: 10px;">
+                                            <div class="card-body">
+                                                <blockquote class="blockquote mb-0" style="height: 120px;">
+                                                    <div style="margin-bottom: 10px;">
+                                                        <span>${name}</span> 
+                                                        <span style="text-align: right; font-size: 12px">(${time})</span>
+                                                    </div>
+                                                    <span>${content}</span>
+                                                </blockquote>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    `
+                    $('#backend-board-list').prepend(temp_html)
+                }
+            }
+        }
+    });
+}
+
+function showFrontendBoard() {
+    $('#frontend-board-list').empty()
+    $.ajax({
+        type: "GET",
+        url: "/board",
+        data: {},
+        success: function (response) {
+            console.log('success_showBackendBoard')
+            let board_list = JSON.parse(response)
+            for (let i = 0; i < board_list.length; i++) {
+                let name = board_list[i][0]
+                let content = board_list[i][1]
+                let time = board_list[i][2]
+                let user_name = board_list[i][3]
+                let category_name = board_list[i][4]
+                if (category_name === '프론트엔드 개발자') {
+                    let temp_html = `
+                                    <div>
+                                        <div style="display: flex; align-items: center;">
+                                                <img alt="이미지가 없습니다" class="img-fluid rounded-start"
+                                                     src="/static/images/default_image.png"
+                                                     style="height: 40px; margin-right: 10px;">
+                                                <span>${user_name}</span> 
+                                        </div>
+                                        <div class="card" style="margin-bottom: 10px;">
+                                            <div class="card-body">
+                                                <blockquote class="blockquote mb-0" style="height: 120px;">
+                                                    <div style="margin-bottom: 10px;">
+                                                        <span>${name}</span> 
+                                                        <span style="text-align: right; font-size: 12px">(${time})</span>
+                                                    </div>
+                                                    <span>${content}</span>
+                                                </blockquote>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    `
+                    $('#frontend-board-list').prepend(temp_html)
+                }
             }
         }
     });
