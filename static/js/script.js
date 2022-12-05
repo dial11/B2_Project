ffce495448f37f4b2ec058c388841fd7198c4396
Joon Ki Chun <joon0858@naver.com>
2022-12-06 06:05:07 +0900

    Update category

--- static/js/script.js
+++ static/js/script.js
@@ -1,33 +1,8 @@
 $(document).ready(function () {
-    $('#all-board-list').empty()
+    showCategories();
     showRandomUser();
-    showBoard(1);
-    showBackendBoard();
-    showFrontendBoard();
+    showBoard('all', 1, 1);
 });
-
-function pickThreeNum(user_length) {
-    let num_list = [];
-    let i = 0;
-    while (i < 3) {
-        let n = Math.floor(Math.random() * user_length);
-        if (!same_num(n)) {
-            num_list.push(n);
-            i++;
-        }
-    }
-
-    function same_num(n) {
-        for (let i = 0; i < num_list.length; i++) {
-            if (n === num_list[i]) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    return num_list;
-}
 
 function showRandomUser() {
     $('#random-user').empty()
@@ -41,8 +16,6 @@ function showRandomUser() {
             let rand_num_list = pickThreeNum(user_list.length)
             for (let i = 0; i < 3; i++) {
                 let rand_num = rand_num_list[i];
-                // let email = user_list[rand_num][0]
-                // let password = user_list[rand_num][1]
                 let name = user_list[rand_num][2]
                 let description = user_list[rand_num][3]
                 let temp_html = `
@@ -67,10 +40,34 @@ function showRandomUser() {
     });
 }
 
-function showBoard(i) {
+function showCategories() {
+    $('#categories').empty()
     $.ajax({
         type: "GET",
-        url: `/board/${i}`,
+        url: "/category",
+        data: {},
+        success: function (response) {
+            console.log('success_showCategories')
+            let category_list = JSON.parse(response)
+            for (let i = 0; i < category_list.length; i++) {
+                let name = category_list[i]
+                let temp_html = `
+                                 <li style="margin-top:10px; margin-bottom: 10px;"><a href="javascript:void(0);" onclick="showBoard('${name}', 1, 1)" style="cursor: pointer;">${name}</a></li>
+                                `
+                $('#categories').append(temp_html)
+            }
+        }
+    });
+}
+
+
+function showBoard(category, page, have_to_reset) {
+    if (have_to_reset === 1) {
+        $('#all-board-list').empty()
+    }
+    $.ajax({
+        type: "GET",
+        url: `/board/${category}/${page}`,
         data: {},
         success: function (response) {
             console.log('success_showBoard')
@@ -107,102 +104,37 @@ function showBoard(i) {
     });
 }
 
-function showBackendBoard() {
-    $('#backend-board-list').empty()
-    $.ajax({
-        type: "GET",
-        url: "/board",
-        data: {},
-        success: function (response) {
-            console.log('success_showBackendBoard')
-            let board_list = JSON.parse(response)
-            for (let i = 0; i < board_list.length; i++) {
-                let name = board_list[i][0]
-                let content = board_list[i][1]
-                let time = board_list[i][2]
-                let user_name = board_list[i][3]
-                let category_name = board_list[i][4]
-                if (category_name === '백엔드 개발자') {
-                    let temp_html = `
-                                    <div>
-                                        <div style="display: flex; align-items: center;">
-                                                <img alt="이미지가 없습니다" class="img-fluid rounded-start"
-                                                     src="/static/image/default_image.png"
-                                                     style="height: 40px; margin-right: 10px;">
-                                                <span>${user_name}</span> 
-                                        </div>
-                                        <div class="card" style="margin-bottom: 10px;">
-                                            <div class="card-body">
-                                                <blockquote class="blockquote mb-0" style="height: 120px;">
-                                                    <div style="margin-bottom: 10px;">
-                                                        <span>${name}</span> 
-                                                        <span style="text-align: right; font-size: 12px">(${time})</span>
-                                                    </div>
-                                                    <span>${content}</span>
-                                                </blockquote>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    `
-                    $('#backend-board-list').append(temp_html)
-                }
-            }
-        }
-    });
-}
-
-function showFrontendBoard() {
-    $('#frontend-board-list').empty()
-    $.ajax({
-        type: "GET",
-        url: "/board",
-        data: {},
-        success: function (response) {
-            console.log('success_showBackendBoard')
-            let board_list = JSON.parse(response)
-            for (let i = 0; i < board_list.length; i++) {
-                let name = board_list[i][0]
-                let content = board_list[i][1]
-                let time = board_list[i][2]
-                let user_name = board_list[i][3]
-                let category_name = board_list[i][4]
-                if (category_name === '프론트엔드 개발자') {
-                    let temp_html = `
-                                    <div>
-                                        <div style="display: flex; align-items: center;">
-                                                <img alt="이미지가 없습니다" class="img-fluid rounded-start"
-                                                     src="/static/image/default_image.png"
-                                                     style="height: 40px; margin-right: 10px;">
-                                                <span>${user_name}</span> 
-                                        </div>
-                                        <div class="card" style="margin-bottom: 10px;">
-                                            <div class="card-body">
-                                                <blockquote class="blockquote mb-0" style="height: 120px;">
-                                                    <div style="margin-bottom: 10px;">
-                                                        <span>${name}</span> 
-                                                        <span style="text-align: right; font-size: 12px">(${time})</span>
-                                                    </div>
-                                                    <span>${content}</span>
-                                                </blockquote>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    `
-                    $('#frontend-board-list').append(temp_html)
-                }
-            }
-        }
-    });
-}
-
+// 무한 스크롤
 let page = 1;
 $(window).scroll(function () {
     if ((window.innerHeight + window.scrollY + 1) >= document.body.offsetHeight) {
-        // document.getElementById('loading icon').style.display = 'block';
         setTimeout(function () {
-            console.log('page:', ++page)
-            showBoard(page)
+            console.log('page:', ++page);
+            showBoard('all', page, 0);
         }, 500)
-
     }
 });
+
+// 랜덤 유저 3명 뽑기 위한 함수
+function pickThreeNum(user_length) {
+    let num_list = [];
+    let i = 0;
+    while (i < 3) {
+        let n = Math.floor(Math.random() * user_length);
+        if (!same_num(n)) {
+            num_list.push(n);
+            i++;
+        }
+    }
+
+    function same_num(n) {
+        for (let i = 0; i < num_list.length; i++) {
+            if (n === num_list[i]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    return num_list;
+}
