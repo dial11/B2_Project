05f6c5ea7c892597489c968e4345026ab5f2cd54
Joon Ki Chun <joon0858@naver.com>
2022-12-06 18:37:51 +0900

    Update category, scrolling

--- static/js/script.js
+++ static/js/script.js
@@ -1,25 +1,35 @@
+let category_state = 'all'
+let page_state = 1
+
 $(document).ready(function () {
-    showCategories();
-    showRandomUser();
-    showBoard('all', 1, 1);
+  // console.log('document.ready')
+  // console.log('page_state:', page_state)
+  showCategories();
+  showRandomUser();
+  if (window.location.search.substring(2) !== '') {
+    category_state = window.location.search.substring(2)
+    $('#category-title').text(`${category_state.toUpperCase()} 뉴스피드`)
+  }
+  showBoard(category_state, 1, 1);
 });
 
 function showRandomUser() {
-    $('#random-user').empty()
-    $.ajax({
-        type: "GET",
-        url: "/user",
-        data: {},
-        success: function (response) {
-            console.log('success_showRandomUser')
-            let user_list = JSON.parse(response)
-            let rand_num_list = pickThreeNum(user_list.length)
-            for (let i = 0; i < 3; i++) {
-                let rand_num = rand_num_list[i];
-                let name = user_list[rand_num][2]
-                let description = user_list[rand_num][3]
-                let temp_html = `
-                                <div class="card mb-3" style="max-width: 540px; margin-right: 30px">
+  $('#random-user').empty()
+  $.ajax({
+    type: "GET",
+    url: "/user",
+    data: {},
+    success: function (response) {
+      // console.log('success_showRandomUser')
+      let user_list = JSON.parse(response)
+      let rand_num_list = pickThreeNum(user_list.length)
+      for (let i = 0; i < 3; i++) {
+        let rand_num = rand_num_list[i];
+        let name = user_list[rand_num][2]
+        let description = user_list[rand_num][3]
+        let temp_html = `
+                                <div class="card mb-3">
+                                    <a href="">
                                     <div class="row g-0">
                                         <div class="col-md-3">
                                             <img alt="이미지가 없습니다" class="img-fluid rounded-start" style="height: 70px;"
@@ -32,52 +42,61 @@ function showRandomUser() {
                                             </div>
                                         </div>
                                     </div>
+                                    </a>
                                 </div>
                                 `
-                $('#random-user').append(temp_html)
-            }
-        }
-    });
+        $('#random-user').append(temp_html)
+      }
+    }
+  });
 }
 
 function showCategories() {
-    $('#categories').empty()
-    $.ajax({
-        type: "GET",
-        url: "/category",
-        data: {},
-        success: function (response) {
-            console.log('success_showCategories')
-            let category_list = JSON.parse(response)
-            for (let i = 0; i < category_list.length; i++) {
-                let name = category_list[i]
-                let temp_html = `
-                                 <li style="margin-top:10px; margin-bottom: 10px;"><a href="javascript:void(0);" onclick="showBoard('${name}', 1, 1)" style="cursor: pointer;">${name}</a></li>
-                                `
-                $('#categories').append(temp_html)
-            }
-        }
-    });
+  $('#categories').empty()
+  $.ajax({
+    type: "GET",
+    url: "/category-list",
+    data: {},
+    success: function (response) {
+      // console.log('success_showCategories')
+      let category_list = JSON.parse(response)
+      for (let i = 0; i < category_list.length; i++) {
+        let name = category_list[i][0]
+        let name_en = category_list[i][1]
+        let temp_html = `
+                        <div class="items-body-content" id="${name_en}" onclick="location.href='/category?=${name_en}'">${name}</div>                        
+                        `
+        $('#categories').append(temp_html)
+      }
+    }
+  });
 }
 
 
 function showBoard(category, page, have_to_reset) {
-    if (have_to_reset === 1) {
-        $('#all-board-list').empty()
-    }
-    $.ajax({
-        type: "GET",
-        url: `/board/${category}/${page}`,
-        data: {},
-        success: function (response) {
-            console.log('success_showBoard')
-            let board_list = JSON.parse(response)
-            for (let i = 0; i < board_list.length; i++) {
-                let name = board_list[i][0]
-                let content = board_list[i][1]
-                let time = board_list[i][2]
-                let user_name = board_list[i][3]
-                let temp_html = `
+  category_state = `${category}`
+  if (have_to_reset === 1) {
+    $('#all-board-list').empty()
+    $('#category-board-list').empty()
+    page_state = 1
+  }
+  $.ajax({
+    type: "GET",
+    url: `/board/${category}/${page}`,
+    data: {},
+    success: function (response) {
+      // console.log('success_showBoard')
+      let board_list = JSON.parse(response)
+      if (board_list.length < 4) {
+        document.getElementById('loading-icon').style.display = 'none'
+        document.getElementById('loading-message').style.display = 'block'
+      }
+      for (let i = 0; i < board_list.length; i++) {
+        let name = board_list[i][0]
+        let content = board_list[i][1]
+        let time = board_list[i][2]
+        let user_name = board_list[i][3]
+        let temp_html = `
                                 <div>
                                     <div style="display: flex; align-items: center;">
                                             <img alt="이미지가 없습니다" class="img-fluid rounded-start"
@@ -98,43 +117,53 @@ function showBoard(category, page, have_to_reset) {
                                     </div>
                                 </div>
                                 `
-                $('#all-board-list').append(temp_html)
-            }
+        if (category !== 'all') {
+          $('#category-board-list').append(temp_html)
+        } else {
+          $('#all-board-list').append(temp_html)
         }
-    });
+      }
+    }
+  });
 }
 
 // 무한 스크롤
-let page = 1;
 $(window).scroll(function () {
-    if ((window.innerHeight + window.scrollY + 1) >= document.body.offsetHeight) {
-        setTimeout(function () {
-            console.log('page:', ++page);
-            showBoard('all', page, 0);
-        }, 500)
-    }
+  let icon = document.getElementById('loading-icon')
+  let message = document.getElementById('loading-message')
+  if (icon.style.display === 'none') {
+    icon.style.display = 'block'
+    message.style.display = 'none'
+  }
+  if ((window.innerHeight + window.scrollY + 132) >= document.body.offsetHeight) {
+    setTimeout(function () {
+      showBoard(category_state, ++page_state, 0);
+      // console.log('page_state:', category_state, page_state);
+      icon.style.display = 'none'
+    }, 500)
+  }
 });
 
-// 랜덤 유저 3명 뽑기 위한 함수
+// 랜덤 유저 3명 뽑기 함수
 function pickThreeNum(user_length) {
-    let num_list = [];
-    let i = 0;
-    while (i < 3) {
-        let n = Math.floor(Math.random() * user_length);
-        if (!same_num(n)) {
-            num_list.push(n);
-            i++;
-        }
+  let num_list = [];
+  let i = 0;
+  while (i < 3) {
+    let n = Math.floor(Math.random() * user_length);
+    if (!same_num(n)) {
+      num_list.push(n);
+      i++;
     }
+  }
 
-    function same_num(n) {
-        for (let i = 0; i < num_list.length; i++) {
-            if (n === num_list[i]) {
-                return true;
-            }
-        }
-        return false;
+  function same_num(n) {
+    for (let i = 0; i < num_list.length; i++) {
+      if (n === num_list[i]) {
+        return true;
+      }
     }
+    return false;
+  }
 
-    return num_list;
+  return num_list;
 }
